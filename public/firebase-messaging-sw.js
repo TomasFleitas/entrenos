bda41bab320deb2bc8a90a1b0a67f820507930d8
Tomas <tomas.fleitas@inovtech.com.mx>
2024-05-06 20:42:19 -0300

    Change firebase messaging worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,22 +5,55 @@ importScripts(
   'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js',
 );
 
-firebase.initializeApp({
+self.addEventListener('fetch', () => {
+  try {
+    const urlParams = new URLSearchParams(location.search);
+    self.firebaseConfig = Object.fromEntries(urlParams);
+  } catch (err) {
+    console.error('Failed to add event listener', err);
+  }
+});
+
+const defaultConfig = {
   apiKey: 'AIzaSyAPmVDNhCZIE6rTseGOp8gz8xq43jGH6OM',
   projectId: 'entrenos-c3514',
   messagingSenderId: '902553761023',
   appId: '1:902553761023:web:e3ec3ab7e256c4294d08ca',
-});
+};
 
-const messaging = firebase.messaging();
+firebase.initializeApp(self.firebaseConfig || defaultConfig);
 
-messaging.onBackgroundMessage(function (payload) {
-  const notificationTitle = payload.notification.title;
-  
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icons/icon-192x192.png',
-  };
+let messaging;
+try {
+  messaging = firebase.messaging.isSupported() ? firebase.messaging() : null;
+} catch (err) {
+  console.error('Failed to initialize Firebase Messaging', err);
+}
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+if (messaging) {
+  try {
+    messaging.onBackgroundMessage((payload) => {
+      const notificationTitle = payload.notification.title;
+
+      const notificationOptions = {
+        body: payload.notification.body,
+        tag: notificationTitle,
+        icon: payload.notification?.image,
+        data: {
+          url: payload?.data?.openUrl,
+        },
+      };
+
+      if (payload?.collapseKey && payload.notification?.image) {
+        self.registration.showNotification(
+          notificationTitle,
+          notificationOptions,
+        );
+      } else {
+        return new Promise(function (resolve, reject) {});
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
